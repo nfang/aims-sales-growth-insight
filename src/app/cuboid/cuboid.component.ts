1a28434fb8a3d21153eaa927e0ba3253d486b4eb
Nathan Fang <nyyrikkifang@gmail.com>
2016-09-18 20:17:34 +1000

    style: paint boards (#7)

--- src/app/cuboid/cuboid.component.ts
+++ src/app/cuboid/cuboid.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+const Color = require('color');
+
 @Component({
   selector: 'cuboid',
   templateUrl: './cuboid.component.html',
@@ -7,9 +9,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CuboidComponent implements OnInit {
 
+  @Input() color: string;
+  @Input() highlight: boolean;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  get frontColor(): string {
+    return this.highlight ? this.lighten(this.color, .5) : this.color;
   }
 
+  get sideColor(): string {
+    let sideColor = Color(this.color).darken(.2).hexString();
+    return this.highlight ? this.lighten(sideColor, .5) : sideColor;
+  }
+
+  private lighten(color: string, amount: number): string {
+    return Color(color).lighten(amount).hexString();
+  }
 }
