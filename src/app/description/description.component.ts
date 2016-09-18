8dbbf8cf78d7cde3ee95ca1d422e63753ab70600
Nathan Fang <nyyrikkifang@gmail.com>
2016-09-18 13:32:38 +1000

    style: adjust description animation

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -10,11 +10,11 @@ import { animate, state, style, transition, trigger } from '@angular/core';
       state('in', style({ transform: 'translateX(0)' })),
       state('out', style({ transform: 'translateX(100%)' })),
       transition('out => in', [
-        animate('400ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+        animate('400ms cubic-bezier(0.0, 0.0, 0.2, 1)')
       ]),
       transition('in => out', [
         style({ opacity: 1 }),
-        animate('200ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+        animate('200ms cubic-bezier(0.4, 0.0, 1, 1)',
           style({
             opacity: 0,
             transform: 'translateY(100%)'
